a25a22b414e83c0ea13858c773aa0183abbcccb1
prisabarvadiya78-alt <prisabarvadiya78@gmail.com>
2026-01-27 07:49:10 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,51 @@
+#arithmetic operator
+a=5
+b=10
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+
+#assignment operator
+x=10
+print(x)
+
+#unary minus
+a=5
+print(-a)
+
+
+#relational operators
+p=10
+q=5
+print(p>q)
+print(p==q)
+print(p<q)
+
+#logical operators
+x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x)
+
+#boolean values
+is_pass=True
+print(is_pass)
+
+#bitwise operators
+a=5
+b=3
+print(a & b)
+print(a | b)
+
+#membership operators
+numbers=[1,2,3,4]
+print( 2 in numbers)
+print(5 not in numbers)
+
+#identity operators
+x=10
+y=10
+print(x is y)
+print(x is not y)
