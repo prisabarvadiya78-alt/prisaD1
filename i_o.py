115b681f3bafa751c78a15373183c656241ef775
prisabarvadiya78-alt <prisabarvadiya78@gmail.com>
2026-02-03 07:51:33 +0530

    Add files via upload

---
+++ i_o.py
@@ -0,0 +1,34 @@
+#print
+print("hello world")
+print("welcome atmiya")
+ 
+ 
+#simple string input
+name=input("enter your name:")
+print("hello",name)
+name=input("enter your name:")
+print("hello",name)
+
+#integer input using int()
+num=int(input("enter an integer:"))
+print("number=",num)
+num=int(input("enter an integer:"))
+print("number=",num)
+
+#floating point input using 
+price=float(input("enter price:"))
+print("price entered:",price)
+price=float(input("enter price:"))
+print("price entered:",price)
+
+
+#boolean
+is_result=True
+print(is_result)
+is_result=False
+print(is_result)
+
+#two inputs one by one
+first=input("enter first word:")
+second=input("enter second word:")
+print("you typed:",first,"and",second)
