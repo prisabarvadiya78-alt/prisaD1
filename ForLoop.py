d002b03938f6534789683091b2f1be84d9857114
prisabarvadiya78-alt <prisabarvadiya78@gmail.com>
2026-02-14 08:24:31 +0530

    Add files via upload

---
+++ ForLoop.py
@@ -0,0 +1,51 @@
+
+#for loop
+
+# EXAMPLE:::
+#[1]print numbers from 1 to 5
+for i in range(1,6):
+    print(i)
+
+
+#[2]print a message 3 times
+for i in range(3):
+    print("Hello")
+
+ 
+
+#PRATICE EXAMPLE:::
+
+#(1)print numbers from 1 to 10
+for i in range(1,11):
+    print(i)
+
+#(2)print even numbers from 1 to 20
+for i in range(1,21):
+    if i % 2==0:
+        print(i)
+
+#(3)print odd numbers from 1 to 15
+for i in range(1,16):
+    if i % 2 !=0:
+        print(i)
+
+#(4)print table of 5
+for i in range(1,11):
+    print("5 x",i,"=",5*i)
+
+#(5)print characters of a string
+name="Atmiya" 
+for letter in name:
+    print(letter)
+
+#(6)sum of numbers from 1 to 5
+total=0
+for i in range(1,6):
+    total= total+i
+    print("sum is:",total)
+
+#(7)print list elements
+numbers=[10,20,30,40]
+for  n in numbers:
+    print(n)   
+
