ad5e5336b0547dfa1d3f7abeee3e109a19dcbeb0
prisabarvadiya78-alt <prisabarvadiya78@gmail.com>
2026-07-01 07:43:31 +0530

    Delete temp_series.py

--- temp_series.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-temp=pd.Series([32,39,40,42,38,37,38],
-index=["sunday","monday","tuesday","wednesday","thursday","friday","saturday"])
-print(temp)
-print("maximum temp",temp.max())
-print("minimum temp",temp.min())
-print("average temp",temp.mean())
